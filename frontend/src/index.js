d6b5b185a87e9e3332dcd3ce6a78611a8437dd72
Maxime Guerin <maxiguerin348@gmail.com>
2025-05-17 15:39:32 +0200

    modification import leeflet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,4 @@
 import './assets/styles/main.css';
-import 'leaflet/dist/leaflet.css';
 import { renderHomePage } from './pages/Home';
 import { renderBornesMap } from './pages/BornesMap';
 import { renderStatistics } from './pages/Statistiques';
--- frontend/src/pages/BornesMap.js
+++ frontend/src/pages/BornesMap.js
@@ -395,7 +395,6 @@ function renderStatsHeatMap(container) {
                 }
             }).addTo(map);
 
-            // ✅ Légende externe
             const legendContainer = document.getElementById('heatmap-legend');
             const grades = [0, 0.01, 0.05, 0.1, 0.2, 0.35, 0.5, 0.75];
             let legendHTML = `<strong>Légende : ${field}</strong><br>`;
